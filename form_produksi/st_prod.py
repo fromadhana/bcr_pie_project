fea1cb06536fc7100a9289d79515d5e964f5376c
Fromadhana <f.romadhana@gmail.com>
2023-02-25 21:38:58 +0700

    vers_6

--- form_produksi/st_prod.py
+++ form_produksi/st_prod.py
@@ -100,25 +100,26 @@ with st.form(key= "form_produksi", clear_on_submit=True):
 
     #gcp credentials
     credentials = service_account.Credentials.from_service_account_info(
-        st.secrets.gcp_service_account, 
+        st.secrets["gcp_service_account"], 
         scopes=['https://www.googleapis.com/auth/spreadsheets',
                 'https://www.googleapis.com/auth/drive.readonly', 
                 'https://spreadsheets.google.com/feeds'])
     #shillelagh
     connection = connect(":memory:", adapter_kwargs={
-     "gsheetsapi": {
+     "gsheetsapi" : {
      "service_account_info": {
-     "type": st.secrets["gcp_service_account"]["type"],
-     "project_id" : st.secrets["gcp_service_account"]["project_id"],
-     "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
-     "private_key" : st.secrets["gcp_service_account"]["private_key"],
-     "client_email" : st.secrets["gcp_service_account"]["client_email"],
-     "client_id" : st.secrets["gcp_service_account"]["client_id"],
-     "auth_uri" : st.secrets["gcp_service_account"]["auth_uri"],
-     "token_uri" : st.secrets["gcp_service_account"]["token_uri"],
-     "auth_provider_x509_cert_url" : st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-     "client_x509_cert_url" : st.secrets["gcp_service_account"]["client_x509_cert_url"]},
-     "subject": "gspread-service@bcr-project-378609.iam.gserviceaccount.com",},},)
+     
+              "type" : st.secrets["gcp_service_account"]["type"],
+              "project_id" : st.secrets["gcp_service_account"]["project_id"],
+              "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
+              "private_key" : st.secrets["gcp_service_account"]["private_key"],
+              "client_email" : st.secrets["gcp_service_account"]["client_email"],
+              "client_id" : st.secrets["gcp_service_account"]["client_id"],
+              "auth_uri" : st.secrets["gcp_service_account"]["auth_uri"],
+              "token_uri" : st.secrets["gcp_service_account"]["token_uri"],
+              "auth_provider_x509_cert_url" : st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+              "client_x509_cert_url" : st.secrets["gcp_service_account"]["client_x509_cert_url"]},
+              "subject" : "gspread-service@bcr-project-378609.iam.gserviceaccount.com",},},)
     
     #load to gsheet
     cursor = connection.cursor()
